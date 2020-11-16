26ac781c48e4d62900285f9239c0fea0ecc683a4
Ubuntu <ubuntu@ip-172-26-12-22.ap-south-1.compute.internal>
2020-11-16 06:31:24 +0000

    some server changes

--- src/asgi.py
+++ src/asgi.py
@@ -1,15 +1,22 @@
 # mysite/asgi.py
 import os
 
+from django.conf.urls import url
+from django.core.asgi import get_asgi_application
+
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+
 import chatapp.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatapp.settings")
-
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
+  "http": django_asgi_app,
   "websocket": AuthMiddlewareStack(
         URLRouter(
             chatapp.routing.websocket_urlpatterns
